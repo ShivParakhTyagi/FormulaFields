85751e61e42be4c91f08d816487b5b923011f62d
ShivParakhTyagi <tyagi_shivparakh00@live.com>
2016-07-26 18:13:21 +0530

    tests

--- EntityFrameworkToSql_ConsoleApp/EntityFrameworkToSql_ConsoleApp/Program.cs
+++ EntityFrameworkToSql_ConsoleApp/EntityFrameworkToSql_ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityFrameworkToSql_ConsoleApp.Model;
+using EntityFrameworkToSql_ConsoleApp.Services;
 
 namespace EntityFrameworkToSql_ConsoleApp
 {
@@ -10,6 +13,141 @@ namespace EntityFrameworkToSql_ConsoleApp
     {
         static void Main(string[] args)
         {
+            EntityModel x = new EntityModel();
+            //x.runDb();
+            //x.runDb_roleAssignment();
+            SqlModel y = new SqlModel();
+            y.runDb().Wait();
+        }
+    }
+
+    class EntityModel
+    {
+        public void runDb()
+        {
+            try
+            {
+                var go=Console.ReadLine();
+                if (go!=null&&go.Contains("N"))
+                {
+                    return;
+                }
+                using (var db = new MobilizeDbSchema())
+                {
+                    var R = db.Users?.ToList();
+                    if (R == null)
+                    {
+                        return;
+                    }
+                    var user = new User()
+                    {
+                        UserLogin = new Login()
+                        {
+                            LoginId = "abc",
+                            Password = "abc",
+                        },
+                        Name = "abc",
+                    };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    Console.WriteLine(user.UserId);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        public void runDb_roleAssignment()
+        {
+            try
+            {
+
+                using (var db = new MobilizeDbSchema())
+                {
+                    var res = (from u in db.Users where u.UserId == 1 select u).FirstOrDefault();
+
+                    var user = res;
+                    var role = new Role()
+                    {
+                        RoleName = "role_abc",
+                        OrganizationId = 1,
+                        RoleDescription = "desc",
+                        RoleOrganization = res.UserOrganization,
+                    };
+                    db.Roles.Add(role);
+
+                    var role2 = new Role()
+                    {
+                        RoleName = "rolabce_abc",
+                        OrganizationId = 1,
+                        RoleDescription = "desc",
+                        RoleOrganization = res.UserOrganization,
+                    };
+                    db.Roles.Add(role);
+
+                    res.UserRoles.Add(role);
+                    res.UserRoles.Add(role2);
+                    db.SaveChanges();
+                    Console.WriteLine(user.UserId);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    class SqlModel
+    {
+        private string connectionString =
+            "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\IMentor\\Source\\Repos\\FormulaFields\\EntityFrameworkToSql_ConsoleApp\\EntityFrameworkToSql_ConsoleApp\\Database\\localDb.mdf;";
+
+        public async Task runDb()
+        {
+            try
+            {
+                var go = Console.ReadLine();
+                if (go != null && go.Contains("N"))
+                {
+                    return;
+                }
+                SqlService service = new SqlService();
+
+                var user = new User()
+                {
+                    UserLogin = new Login()
+                    {
+                        LoginId = "abc",
+                        Password = "abc",
+                    },
+                    Name = "abc",
+                };
+
+                Console.WriteLine(user.Name);
+                var sqlStatement =
+                    $"SELECT U.*, R.*, UR.*, L.*, O.* " +
+                    $"FROM Users " + 
+                    $"   AS U" +
+                    $"   LEFT JOIN Logins AS L ON U.UserLogin_LoginId  = L.LoginId " +
+                    $"   LEFT JOIN Organizations AS O ON U.OrganizationId  = O.OrganizationId " +
+                    $"   LEFT JOIN UserRoles AS UR ON U.UserId  = UR.User_UserId " +
+                    $"   LEFT JOIN Roles AS R ON UR.Role_RoleId  = R.RoleId " +
+                    $" WHERE U.Name='abc' ;";
+                    //$" WHERE U.UserId = @UserId";
+                var x = "Select * from Users where Name='abc'";
+                var result1 = await service.GetDataAsync(connectionString, sqlStatement);
+
+                var result = await service.GetDataAsync<User>(connectionString, sqlStatement);
+
+                Console.WriteLine(result.UserId);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ EntityFrameworkToSql_ConsoleApp/EntityFrameworkToSql_ConsoleApp/Services/SqlService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkToSql_ConsoleApp.Model;
+
+namespace EntityFrameworkToSql_ConsoleApp.Services
+{
+    public class SqlService
+    {
+
+
+        public async Task<T> GetDataAsync<T>(string connectionString, string sqlCommand) where T : new()
+        {
+            using (var con = new SqlConnection(connectionString))
+            {
+                await con.OpenAsync();
+
+                var command = con.CreateCommand();
+                command.CommandText = sqlCommand;
+                command.CommandType = CommandType.Text;
+                var reader = await command.ExecuteReaderAsync();
+
+                var type = typeof(T);
+
+                var obj = Activator.CreateInstance(type);
+
+                while (reader.Read())
+                {
+                    foreach (var p in type.GetProperties())
+                    {
+                        if (p.PropertyType.Namespace == typeof(User).Namespace)
+                        {
+                            var val = await Construct(reader, p.PropertyType);
+                            p.SetValue(obj, val);
+                            continue;
+                        }
+                        if (!p.PropertyType.IsInterface)
+                        {
+                            p.SetValue(obj, reader[p.Name] == null || reader[p.Name] == DBNull.Value ? null : reader[p.Name]);
+                        }
+
+                        if (p.PropertyType.IsInterface)
+                        {
+                            // this is some sort of interface property
+                            var val =p.GetValue(obj);
+                            var valType = val.GetType();
+                            Console.WriteLine(valType);
+                            //p.SetValue(obj, reader[p.Name] == null || reader[p.Name] == DBNull.Value ? null : reader[p.Name]);
+                        }
+                    }
+                }
+                reader.Close();
+                return (T)obj;
+
+            }
+
+        }
+
+        private async Task<object> Construct(SqlDataReader reader,Type type)
+        {
+
+            var obj = Activator.CreateInstance(type);
+            
+            {
+                foreach (var p in type.GetProperties())
+                {
+
+                    if (p.PropertyType.Namespace == typeof(User).Namespace)
+                    {
+                        var val = await Construct(reader, p.PropertyType);
+                        p.SetValue(obj, val);
+                        continue;
+                    }
+                    if (!p.PropertyType.IsInterface)
+                    {
+                        p.SetValue(obj, reader[p.Name] == null || reader[p.Name] == DBNull.Value ? null : reader[p.Name]);
+                    }
+                }
+            }
+            return obj;
+        }
+
+        public async Task<List<Dictionary<string, object>>> GetDataAsync(string connectionString, string sqlCommand)
+        {
+            try
+            {
+                using (var con = new SqlConnection(connectionString))
+                {
+                    await con.OpenAsync();
+
+                    var command = con.CreateCommand();
+                    command.CommandText = sqlCommand;
+                    command.CommandType = CommandType.Text;
+                    var reader = await command.ExecuteReaderAsync();
+
+                    var listOfDictionary = new List<Dictionary<string, object>>();
+
+                    while (reader.Read())
+                    {
+                        var dictionary = new Dictionary<string, object>();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            var name = reader.GetName(i);
+                            if(!dictionary.ContainsKey(name))
+                            dictionary.Add(name,
+                                reader[i] == null || reader[i] == DBNull.Value ? null : reader[i]);
+                        }
+                        listOfDictionary.Add(dictionary);
+                    }
+                    reader.Close();
+                    return listOfDictionary;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("ExeptionAt-SqlService.cs-GetDataAsync()-" + ex.Message);
+                return null;
+            }
+        }
+
+    }
+}
